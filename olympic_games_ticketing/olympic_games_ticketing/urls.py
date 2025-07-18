b6c4721f66065da5de1f8d35ac8a0288d78c6ecd
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-07-18 18:27:23 +0200

    Ajout des urls pour la page d'accueil et inclusion des urls de l'application accounts

--- olympic_games_ticketing/olympic_games_ticketing/urls.py
+++ olympic_games_ticketing/olympic_games_ticketing/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from olympic_games_ticketing.views import home_page
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", home_page, name="home"),
+    path("accounts/", include("accounts.urls")),
 ]
