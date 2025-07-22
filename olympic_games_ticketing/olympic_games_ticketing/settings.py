b98741964f11d81d0e7d0c5f8ebbfa3f9bb1258f
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-07-22 15:00:22 +0200

    Import et configuration python-dotenv

--- olympic_games_ticketing/olympic_games_ticketing/settings.py
+++ olympic_games_ticketing/olympic_games_ticketing/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+# Allows environment variables to be used in the development environment.
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
