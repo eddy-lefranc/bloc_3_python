1a3616ee1b13fffea3d1024030b73cd35a9d798e
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-07-25 21:03:41 +0200

    Suppression de l'url signup-confirmation

--- olympic_games_ticketing/accounts/urls.py
+++ olympic_games_ticketing/accounts/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from accounts.views import signup_confirmation_page, signup_page
+from accounts.views import signup_page
 
 urlpatterns = [
     path("signup/", signup_page, name="signup"),
-    path("signup/confirmation/", signup_confirmation_page, name="signup-confirmation"),
 ]
