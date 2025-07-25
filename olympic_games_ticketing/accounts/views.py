172e436c10d40fd2ee11122c034390abea647fff
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-07-25 21:02:59 +0200

    Modification fonction signup_page pour ajouter un message en cas de confirmation d'inscription sur la page signup + suppression de la fonction signup_confirmation_page

--- olympic_games_ticketing/accounts/views.py
+++ olympic_games_ticketing/accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import redirect, render
 
 from accounts.forms import SignupForm
@@ -10,7 +11,7 @@ def signup_page(request):
     If the user is already authenticated, redirects to the home page.
     On GET requests, renders an empty signup form.
     On POST requests, validates the submitted data:
-    - If valid, creates the user and redirects to the signup confirmation page.
+    - If valid, creates the user and display a success message on the page.
     - If invalid, re-renders the form with error messages.
     """
 
@@ -21,18 +22,11 @@ def signup_page(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("signup-confirmation")
+            messages.success(
+                request,
+                "🥇 Inscription réussie ! Connectez-vous dès maintenant pour accéder à votre compte.",
+            )
     else:
         form = SignupForm()
 
     return render(request, "accounts/signup.html", context={"form": form})
-
-
-def signup_confirmation_page(request):
-    """
-    Render the signup confirmation page.
-
-    Displays a simple confirmation message after successful registration.
-    """
-
-    return render(request, "accounts/signup-confirmation.html")
