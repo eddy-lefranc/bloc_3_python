a48a033d45df4d88ec6933102464f1adc3140d49
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-08-03 17:16:32 +0200

    Ajout des tests pour les champs last_name et registration_key

--- olympic_games_ticketing/accounts/tests/test_models.py
+++ olympic_games_ticketing/accounts/tests/test_models.py
@@ -69,3 +69,34 @@ class UserModelTest(TestCase):
             "first_name"
         ).verbose_name
         self.assertEqual(first_name_field_verbose_name, "Prénom")
+
+    def test_last_name_field_is_in_required_fields_constant(self):
+        """Test that 'last_name' is included in the REQUIRED_FIELDS constant."""
+        self.assertIn("last_name", self.user.REQUIRED_FIELDS)
+
+    def test_last_name_field_max_length(self):
+        """Test that the max length for the last name field is 150."""
+        last_name_field_max_length = self.user._meta.get_field("last_name").max_length
+        self.assertEqual(last_name_field_max_length, 150)
+
+    def test_last_name_field_verbose_name(self):
+        """Test that the last name field verbose name is 'Nom'."""
+        last_name_field_verbose_name = self.user._meta.get_field(
+            "last_name"
+        ).verbose_name
+        self.assertEqual(last_name_field_verbose_name, "Nom")
+
+    def test_registration_key_field_is_generated(self):
+        """Test that a registration key is generated for the user."""
+        self.assertIsNotNone(self.user.registration_key)
+
+    def test_registration_key_field_verbose_name(self):
+        """Test that the registration key field verbose name is 'Clé d'inscription'."""
+        registration_key_field_verbose_name = self.user._meta.get_field(
+            "registration_key"
+        ).verbose_name
+        self.assertEqual(registration_key_field_verbose_name, "Clé d'inscription")
+
+    def test_str_method_returns_full_name(self):
+        """Test that the __str__ method returns the full name of the user."""
+        self.assertEqual(str(self.user), "John Doe")
