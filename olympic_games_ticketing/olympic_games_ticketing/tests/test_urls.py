6d3305578975dd56117c66bdbb694c1066e29359
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-08-05 17:24:43 +0200

    Ajout des tests pour les URLS du projet de base Django

---
+++ olympic_games_ticketing/olympic_games_ticketing/tests/test_urls.py
@@ -0,0 +1,20 @@
+from django.test import SimpleTestCase
+from django.urls import resolve
+
+from olympic_games_ticketing.views import home_page
+
+
+class BaseProjectUrlsTests(SimpleTestCase):
+    """Test cases for verifying that the base project URLS are configured correctly."""
+
+    def setUp(self):
+        """Resolve the '/' URL for tests."""
+        self.match = resolve("/")
+
+    def test_base_url_resolves_to_correct_view(self):
+        """Ensure that '/' URL resolves to the home_page view."""
+        self.assertEqual(self.match.func, home_page)
+
+    def test_base_url_resolves_to_correct_name(self):
+        """Ensure that '/' URL has the correct URL name 'home'."""
+        self.assertEqual(self.match.url_name, "home")
