1b70839cb472b527156484a51edadfc59750b6c3
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-08-05 17:28:42 +0200

    Ajout des tests pour les URLS de l'application accounts du projet

---
+++ olympic_games_ticketing/accounts/tests/test_urls.py
@@ -0,0 +1,20 @@
+from django.test import SimpleTestCase
+from django.urls import resolve
+
+from accounts.views import signup_page
+
+
+class AccountsAppUrlsTests(SimpleTestCase):
+    """Test cases for verifying that accounts app URLs are configured correctly."""
+
+    def setUp(self):
+        """Resolve the '/accounts/signup/' URL for tests."""
+        self.match = resolve("/accounts/signup/")
+
+    def test_signup_url_resolves_to_correct_view(self):
+        """Ensure that '/accounts/signup/' URL resolves to the signup_page view."""
+        self.assertEqual(self.match.func, signup_page)
+
+    def test_signup_url_resolves_to_correct_name(self):
+        """Ensure that '/accounts/signup/' URL has the correct URL name 'signup'."""
+        self.assertEqual(self.match.url_name, "signup")
