6556447fcba0a32c28ee230143f35d4425f53d69
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-08-13 16:34:19 +0200

    Modification du nom de la classe de tests

--- olympic_games_ticketing/accounts/tests/test_urls.py
+++ olympic_games_ticketing/accounts/tests/test_urls.py
@@ -4,7 +4,7 @@ from django.urls import resolve
 from accounts.views import signup_page
 
 
-class AccountsAppUrlsTests(SimpleTestCase):
+class TestAccountsAppUrlsTestCase(SimpleTestCase):
     """Test cases for verifying that accounts app URLs are configured correctly."""
 
     def setUp(self):
