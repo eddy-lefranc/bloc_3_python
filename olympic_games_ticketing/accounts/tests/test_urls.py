df76d307586068dcc0e312624f91f3095f94a2d3
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-08-14 17:18:46 +0200

    Modification des tests pour couvrir l'URL de confirmation d'inscription

--- olympic_games_ticketing/accounts/tests/test_urls.py
+++ olympic_games_ticketing/accounts/tests/test_urls.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import resolve
 
-from accounts.views import signup_page
+from accounts.views import signup_confirmation_page, signup_page
 
 
 class TestAccountsAppUrls(SimpleTestCase):
@@ -9,12 +9,21 @@ class TestAccountsAppUrls(SimpleTestCase):
 
     def setUp(self):
         """Resolve the '/accounts/signup/' URL for tests."""
-        self.match = resolve("/accounts/signup/")
+        self.match_signup = resolve("/accounts/signup/")
+        self.match_signup_confirmation = resolve("/accounts/signup/confirmation/")
 
     def test_signup_url_resolves_to_correct_view(self):
         """Ensure that '/accounts/signup/' URL resolves to the signup_page view."""
-        self.assertEqual(self.match.func, signup_page)
+        self.assertEqual(self.match_signup.func, signup_page)
 
     def test_signup_url_resolves_to_correct_name(self):
         """Ensure that '/accounts/signup/' URL has the correct URL name 'signup'."""
-        self.assertEqual(self.match.url_name, "signup")
+        self.assertEqual(self.match_signup.url_name, "signup")
+
+    def test_signup_confirmation_url_resolves_to_correct_view(self):
+        """Ensure that '/accounts/signup/confirmation/' URL resolves to the signup_page view."""
+        self.assertEqual(self.match_signup_confirmation.func, signup_confirmation_page)
+
+    def test_signup_confirmation_url_resolves_to_correct_name(self):
+        """Ensure that '/accounts/signup/confirmation/' URL has the correct URL name 'signup-confirmation'."""
+        self.assertEqual(self.match_signup_confirmation.url_name, "signup-confirmation")
