b1e6077d612fb49c5ae987507d5ad78070fbe3b4
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-08-15 17:51:27 +0200

    Configuratoin ALLOWED_HOSTS et CSRF_TRUSTED_ORIGINS pour déploiement

--- olympic_games_ticketing/olympic_games_ticketing/settings.py
+++ olympic_games_ticketing/olympic_games_ticketing/settings.py
@@ -36,7 +36,16 @@ SECRET_KEY = os.environ.get(
 # DEBUG = True
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "web-production-fae7.up.railway.app",
+    "127.0.0.1",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://web-production-fae7.up.railway.app",
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+]
 
 
 # Application definition
