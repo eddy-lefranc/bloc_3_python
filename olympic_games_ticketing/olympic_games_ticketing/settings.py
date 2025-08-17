9ee067a065993f2e6c970288f72f8ee4cf45b085
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-08-17 15:22:07 +0200

    Modification utilisation BDD pour séparer dev et prod + secret key utilisée en ENV

--- olympic_games_ticketing/olympic_games_ticketing/settings.py
+++ olympic_games_ticketing/olympic_games_ticketing/settings.py
@@ -27,10 +27,7 @@ load_dotenv(env_path)
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET_KEY",
-    "django-insecure-vwerckyh9f5d4dddi@hf$!xb_ws-g^u!fd*g9o-%ke=cmdfs21",
-)
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
@@ -94,12 +91,22 @@ WSGI_APPLICATION = "olympic_games_ticketing.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+# Update database configuration from $DATABASE_URL environment variable (if defined)
+
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        "default": dj_database_url.config(
+            conn_max_age=500,
+            conn_health_checks=True,
+        )
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
@@ -163,11 +170,3 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Customizing authentication
 
 AUTH_USER_MODEL = "accounts.User"
-
-# Update database configuration from $DATABASE_URL environment variable (if defined)
-
-if "DATABASE_URL" in os.environ:
-    DATABASES["default"] = dj_database_url.config(
-        conn_max_age=500,
-        conn_health_checks=True,
-    )
