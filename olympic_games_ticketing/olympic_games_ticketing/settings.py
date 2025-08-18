64151b843a33a019b47227192b523b48e138a0bd
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-08-18 16:23:53 +0200

    Suppression commentaire obsolète

--- olympic_games_ticketing/olympic_games_ticketing/settings.py
+++ olympic_games_ticketing/olympic_games_ticketing/settings.py
@@ -30,7 +30,6 @@ load_dotenv(env_path)
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = [
