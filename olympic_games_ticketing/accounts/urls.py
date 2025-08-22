9e741d4074c037949d40fa54b52845d64bc09056
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-08-22 17:56:15 +0200

    Ajout URL login

--- olympic_games_ticketing/accounts/urls.py
+++ olympic_games_ticketing/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from accounts.views import signup_confirmation_page, signup_page
+from accounts.views import login_page, signup_confirmation_page, signup_page
 
 urlpatterns = [
     path("signup/", signup_page, name="signup"),
     path("signup/confirmation/", signup_confirmation_page, name="signup-confirmation"),
+    path("login", login_page, name="login"),
 ]
