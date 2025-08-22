b66dd1d783109ae89dcc2ea32796e1dfab8b724e
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-08-22 20:56:56 +0200

    Correction nom URL logout

--- olympic_games_ticketing/accounts/urls.py
+++ olympic_games_ticketing/accounts/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path("signup/", signup_page, name="signup"),
     path("signup/confirmation/", signup_confirmation_page, name="signup-confirmation"),
     path("login/", login_page, name="login"),
-    path("logout/", logout_user, name="login"),
+    path("logout/", logout_user, name="logout"),
 ]
