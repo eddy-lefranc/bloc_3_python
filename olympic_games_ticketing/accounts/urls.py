d7d0b76cdc5894aa6fad53a4ade7cb3c4ab46f4c
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-08-22 18:22:58 +0200

    Correction typographie

--- olympic_games_ticketing/accounts/urls.py
+++ olympic_games_ticketing/accounts/urls.py
@@ -5,5 +5,5 @@ from accounts.views import login_page, signup_confirmation_page, signup_page
 urlpatterns = [
     path("signup/", signup_page, name="signup"),
     path("signup/confirmation/", signup_confirmation_page, name="signup-confirmation"),
-    path("login", login_page, name="login"),
+    path("login/", login_page, name="login"),
 ]
