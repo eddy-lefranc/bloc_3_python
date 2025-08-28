11a95ad2d0516ed410bfc9887b924c67bec271f3
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-08-28 20:57:36 +0200

    Ajout de docstring pour la classe ProductAdmin

--- olympic_games_ticketing/products/admin.py
+++ olympic_games_ticketing/products/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from products.models import Offer
 
-admin.site.register(Offer)
+
+class ProductAdmin(admin.ModelAdmin):
+    """
+    This ModelAdmin class use a prepopulated_field to avoid manually adding a slug.
+    """
+
+    prepopulated_fields = {"slug": ("name",)}
+
+
+admin.site.register(Offer, ProductAdmin)
