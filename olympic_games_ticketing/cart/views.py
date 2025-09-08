03a72b5e292df2cdcfa86cd77af22cac4bca3e71
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-09-08 18:01:25 +0200

    Ajout vue page récapitulative panier

--- olympic_games_ticketing/cart/views.py
+++ olympic_games_ticketing/cart/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def cart_summary_page(request):
+    """
+    Render the cart summary page.
+
+    This view displays the summary of the user's cart.
+    """
+
+    return render(request, "cart/summary.html")
