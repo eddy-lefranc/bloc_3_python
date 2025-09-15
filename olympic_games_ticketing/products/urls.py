908c80ac30b031409d536a856184aaedd4f9eb62
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-09-15 18:24:52 +0200

    Changement nom vue 'offer_detail_page'

--- olympic_games_ticketing/products/urls.py
+++ olympic_games_ticketing/products/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from products.views import offer_detail, offers_list_page
+from products.views import offer_detail_page, offers_list_page
 
 urlpatterns = [
     path("offers/", offers_list_page, name="offers"),
-    path("offers/<str:slug>/", offer_detail, name="offer"),
+    path("offers/<str:slug>/", offer_detail_page, name="offer"),
 ]
