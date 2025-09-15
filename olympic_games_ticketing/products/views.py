5fa3afd0d4b7fe10d45acf9ee35e020b5ccbcc57
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-09-15 18:25:25 +0200

    Réorganisation du code et ajout vue 'offer_detail_page'

--- olympic_games_ticketing/products/views.py
+++ olympic_games_ticketing/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from products.models import Offer
 
@@ -11,5 +11,19 @@ def offers_list_page(request):
     """
 
     offers = Offer.objects.filter(is_active=True).order_by("seats")
+    context = {"offers": offers}
 
-    return render(request, "products/offers-list.html", {"offers": offers})
+    return render(request, "products/offers-list.html", context)
+
+
+def offer_detail_page(request, slug):
+    """
+    Renders the offer detail page.
+
+    This view displays the details of a single offer identified by its slug.
+    """
+
+    offer = get_object_or_404(Offer, slug=slug)
+    context = {"offer": offer}
+
+    return render(request, "products/offer-detail.html", context)
