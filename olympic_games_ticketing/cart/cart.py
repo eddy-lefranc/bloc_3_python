dfb21c39db7506578bc3a0c836a42b708fe7bf43
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-09-24 17:53:04 +0200

    Ajout des fonctions 'add', '__len__' et 'get_products'

--- olympic_games_ticketing/cart/cart.py
+++ olympic_games_ticketing/cart/cart.py
@@ -1,3 +1,6 @@
+from products.models import Offer
+
+
 class Cart:
     """Manage the shopping cart stored in the user's session."""
 
@@ -11,3 +14,35 @@ class Cart:
             cart = self.session["session_key"] = {}
 
         self.cart = cart
+
+    def add(self, product):
+        """
+        Adds a single product to the basket and stores the relevant information
+        in a dictionary.
+        """
+
+        product_id = str(product.id)
+
+        if product_id not in self.cart:
+            self.cart[product_id] = {
+                "name": product.name,
+                "image": product.thumbnail.url,
+                "seats": product.seats,
+                "price": str(product.price),
+                "quantity": 1,
+            }
+
+        self.session.modified = True
+
+    def __len__(self):
+        """Return the number of items in the cart."""
+
+        return len(self.cart)
+
+    def get_products(self):
+        """Retrieve a QuerySet of Offer instances in the cart."""
+
+        ids = self.cart.keys()
+        products = Offer.objects.filter(id__in=ids)
+
+        return products
