2040c624fd848769e87a7b3820c8c5c4d1d493ef
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-09-24 18:51:09 +0200

    Ajout fonction 'cart_add' + modifications 'cart_summary_page'

--- olympic_games_ticketing/cart/views.py
+++ olympic_games_ticketing/cart/views.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
+from products.models import Offer
+
+from .cart import Cart
 
 
 @login_required
@@ -10,4 +14,29 @@ def cart_summary_page(request):
     This view displays a summary of the logged-in user's shopping cart.
     """
 
-    return render(request, "cart/summary.html")
+    cart = Cart(request)
+    cart_products = cart.get_products()
+
+    return render(request, "cart/summary.html", {"cart_products": cart_products})
+
+
+@login_required
+def cart_add(request):
+    """
+    Add an offer to the shopping cart.
+
+    This view handles AJAX POST requests to add an offer to the cart
+    and returns the updated cart size as JSON.
+    """
+
+    cart = Cart(request)
+
+    if request.POST.get("action") == "post":
+        offer_id = int(request.POST.get("offer_id"))
+        offer = get_object_or_404(Offer, id=offer_id)
+
+        cart.add(offer)
+
+        cart_size = len(cart.cart)
+
+        return JsonResponse({"size": cart_size})
