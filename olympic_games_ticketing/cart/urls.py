0e705de137a3c2d47484a698027132522fb80c4a
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-09-27 14:31:12 +0200

    Ajout de l'url 'cart-add'

--- olympic_games_ticketing/cart/urls.py
+++ olympic_games_ticketing/cart/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from cart.views import cart_add, cart_summary_page
+from cart.views import add_offer_to_cart, cart_summary_page
 
 urlpatterns = [
     path("summary/", cart_summary_page, name="cart"),
-    path("add/", cart_add, name="cart-add"),
+    path("add/", add_offer_to_cart, name="cart-add"),
 ]
