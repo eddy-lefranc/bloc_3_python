60bbbac94b02ecb415adfae07a731b4ce495803c
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-10-13 14:52:55 +0200

    Ajout des URLs d'application 'orders'

--- olympic_games_ticketing/olympic_games_ticketing/urls.py
+++ olympic_games_ticketing/olympic_games_ticketing/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("cart/", include("cart.urls")),
     path("products/", include("products.urls")),
+    path("orders/", include("orders.urls")),
 ]
 
 if settings.DEBUG:
