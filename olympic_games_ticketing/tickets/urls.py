a568e37576dfd2e7d15b3cb1fa4207b04077118f
Eddy Lefranc <eddy-lefranc@outlook.fr>
2025-10-17 15:12:05 +0200

    erreur saisie

--- olympic_games_ticketing/tickets/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import generate_tickets_for_order
-
-app_name = "tickets"
-
-urlpatterns = [
-    path("generate/<int:order_id>/", generate_tickets_for_order, name="generate"),
-]
